79b70b07b6d2ebcba85e6e21dd0ac949debe96da
zsoltvida90-cyber <zsolt.vida90@gmail.com>
2026-08-14 00:52:45 +0200

    test: replace platform contract regression file

--- content_os/tests/test_platform_contract.py
+++
@@ -1,7 +0,0 @@
-from content_os.platforms.platform_contract import normalize_platform,build_x_post,validate_platform_output
-assert normalize_platform('Tik Tok')=='TIKTOK'
-assert normalize_platform('Twitter')=='X'
-assert build_x_post('a'*280)['character_count']==280
-assert validate_platform_output('tiktok','REEL')['compatible'] is True
-assert validate_platform_output('x','CAROUSEL')['compatible'] is False
-print('PASS 5/5')
