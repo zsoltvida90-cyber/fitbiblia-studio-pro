c67d5addc912987f011ed6a7e27507fcc4ba07c4
zsoltvida90-cyber <zsolt.vida90@gmail.com>
2026-08-14 00:52:14 +0200

    refactor: restore platform contract entrypoint

---
+++ content_os/platforms/platform_contract.py
@@ -0,0 +1,8 @@
+from content_os.platforms import platform_contract_v2 as _v2
+
+normalize_platform = _v2.normalize_platform
+build_x_post = _v2.build_x_post
+build_x_thread = _v2.build_x_thread
+validate_platform_output = _v2.validate_platform_output
+x_weighted_length = _v2.x_weighted_length
+X_STANDARD_CHAR_LIMIT = _v2.X_STANDARD_CHAR_LIMIT
