d66e4b26b1652b2afeca6f70364a93a6dda9d939
zsoltvida90-cyber <zsolt.vida90@gmail.com>
2026-08-15 08:55:40 +0200

    Expose external source normalization

--- content_os/research/__init__.py
+++ content_os/research/__init__.py
@@ -1,1 +1,2 @@
-from .library import bundle_filename,classify_freshness,sha256_file,validate_index_record,verify_bundle_hash
+from .library import bundle_filename, classify_freshness, sha256_file, validate_index_record, verify_bundle_hash
+from .external_sources import ExternalSourcePacket, normalize_external_source
